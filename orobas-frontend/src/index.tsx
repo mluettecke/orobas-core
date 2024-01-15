c6866a0b15df049821835b1c6ccc0cfe17694fc3
Marcel L <4482510+mluettecke@users.noreply.github.com>
2024-01-15 21:12:32 +0100

    ✨feat(orobas-frontend): Add tailwind to orobas-frontend

--- orobas-frontend/src/index.tsx
+++ orobas-frontend/src/index.tsx
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom/client";
 
 import App from "@/app";
 
+import "./style.css";
+
 const root = ReactDOM.createRoot(document.getElementById("app") as HTMLElement);
 
 root.render(<App />);
--- orobas-frontend/webpack/webpack.rules.js
+++ orobas-frontend/webpack/webpack.rules.js
@@ -10,4 +10,8 @@ module.exports = [
       },
     },
   },
+  {
+    test: /\.css$/i,
+    use: ["style-loader", "css-loader", "postcss-loader"],
+  },
 ];
