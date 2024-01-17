a437d5bf006f472495e79db82469c7d2fb0b2128
Marcel L <4482510+mluettecke@users.noreply.github.com>
2024-01-17 22:01:17 +0100

    🛠️build(orobas-frontend): Add module federation to webpack config

--- orobas-frontend/webpack/webpack.plugins.js
+++ orobas-frontend/webpack/webpack.plugins.js
@@ -1,6 +1,8 @@
 const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
 const ReactRefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ModuleFederationPlugin =
+  require("webpack").container.ModuleFederationPlugin;
 
 const { isDevelopment } = require("./webpack.helpers");
 
@@ -12,5 +14,20 @@ module.exports = [
     template: "public/index.html",
     inject: true,
     filename: "./index.html",
+  }),
+  new ModuleFederationPlugin({
+    name: "host",
+    shared: {
+      react: {
+        eager: true,
+        singleton: true,
+        strictVersion: true,
+        requiredVersion: "18.2.0",
+      },
+      "react-dom": {
+        eager: true,
+        singleton: true,
+      },
+    },
   }),
 ];
