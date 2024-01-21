aaa82bb68761a4e78d4c04a3b56fd7ebf71d18dc
Marcel L <4482510+mluettecke@users.noreply.github.com>
2024-01-21 21:30:22 +0100

    ✨feat(orobas-utils): Add className prop to Icon component

--- orobas-utils/src/components/Icon/Icon.tsx
+++ orobas-utils/src/components/Icon/Icon.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import { Icon as IconifyIcon } from "@iconify/react";
 
+import { cn } from "@/utils";
+
 type IconProps = {
   name: string;
+  className?: string;
 };
 
 export function Icon(props: IconProps) {
-  return <IconifyIcon icon={props.name} />;
+  return <IconifyIcon icon={props.name} className={cn(props.className)} />;
 }
